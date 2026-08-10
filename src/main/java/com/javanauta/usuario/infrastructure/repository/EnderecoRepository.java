c268dad8e9cbbbbcd26dca1011d1dd34f8951d6f
Augusto Simplicio <simpliciocr7@gmail.com>
2026-08-09 21:01:30 -0300

    adicionados métodos get, delete e login

--- src/main/java/com/javanauta/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/javanauta/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,6 @@
 package com.javanauta.usuario.infrastructure.repository;
 
-import com.augusto.aprendendospring.intraestructure.entity.Endereco;
+import com.javanauta.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
